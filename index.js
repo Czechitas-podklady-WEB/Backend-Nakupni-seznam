6cde763037f965fd2ee936779869aba829598f3e
Filip Chalupa <chalupa.filip@gmail.com>
2020-10-22 21:06:00 +0200

    Get port from environment

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
-const port = 3000
+const port = parseInt(process.env.PORT || '', 10)
+if (!port) {
+	throw new Error('PORT environment variable not set to a number.')
+}
 
 app.get('/', (request, response) => {
 	response.send('Hello')
